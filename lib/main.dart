237f081ba364c72c7dbe89c11505f76c73524e21
unknown <li_mokrane@esi.dz>
2024-12-12 21:24:54 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,164 @@
+import 'package:gdg_school/dataservice.dart';
+import 'package:flutter/material.dart';
+import 'package:gdg_school/secondrouter.dart';
+import 'models.dart';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:gdg_school/dataservice.dart';
+import 'package:gdg_school/models.dart';
+import 'package:gdg_school/secondrouter.dart';
+import 'package:gdg_school/citieslist.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState(); //jddijdisjsji
+}
+
+class _HomeState extends State<Home> {
+  final data_service = DataService();
+
+  final textController = TextEditingController();
+
+  late WeatherResponse? _response = null;
+
+  List<String> list = [];
+  List<double> list_temp = [];
+  List<double> list_max = [];
+  List<double> list_min = [];
+  List<double> list_humidity = [];
+  List<String> list_main = [];
+  List<String> list_icon = [];
+
+  String NullMessage = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color.fromRGBO(236, 244, 252, 1.0),
+        appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: const Color.fromRGBO(236, 244, 252, 1.0),
+          toolbarHeight: 100,
+          actions: [
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.fromLTRB(17, 15, 115, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Location',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: const Color.fromRGBO(74, 90, 129, 10),
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Schyler',
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        'Search your forecast archive',
+                        style: TextStyle(
+                          color: Color.fromRGBO(74, 90, 129, 10),
+                          fontFamily: 'Schyler',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  height: 50,
+                  width: 50,
+                  child: FittedBox(
+                      child: FloatingActionButton(
+                    backgroundColor: Color.fromRGBO(236, 244, 252, 1),
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return SimpleDialog(
+                              contentPadding: const EdgeInsets.all(16.0),
+                              children: [
+                                TextField(
+                                  controller: textController,
+                                ),
+                                const SizedBox(height: 16.0),
+                                ElevatedButton(
+                                    onPressed: () {
+                                      if (textController.text == null) {
+                                        NullMessage = 'enter a city!';
+                                      } else {
+                                        getData();
+                                      }
+                                    },
+                                    child: const Text('Add city')),
+                                Text(NullMessage),
+                              ],
+                            );
+                          });
+                    },
+                    child: const Icon(
+                      Icons.add,
+                      color: Color.fromRGBO(74, 90, 129, 10),
+                    ),
+                  )),
+                )
+              ],
+            )
+          ],
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(20),
+            child: ListView.builder(
+                itemCount: list.length,
+                itemBuilder: (context, index) {
+                  final item = list[index];
+                  return Dismissible(
+                      key: UniqueKey(),
+                      onDismissed: (direction) {
+                        setState(() {
+                          list.removeAt(index);
+                          list_temp.removeAt(index);
+                          list_max.removeAt(index);
+                          list_min.removeAt(index);
+                          list_icon.removeAt(index);
+                        });
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('$item dismissed')));
+                      },
+                      background: Container(
+                        color: Color.fromRGBO(74, 90, 129, 10),
+                      ),
+                      child: City(
+                        cityName: list[index],
+                        temp: list_temp[index],
+                        max: list_max[index],
+                        min: list_min[index],
+                        icon: list_icon[index],
+                      ));
+                })));
+  }
+
+  void getData() async {
+    final response = await data_service.getWeather(textController.text);
+    setState(() {
+      _response = response;
+      list.add(_response!.cityName);
+      list_temp.add(_response!.tempInfo.tempInfo);
+      list_max.add(_response!.tempInfo.maxInfo);
+      list_min.add(_response!.tempInfo.minInfo);
+      list_icon.add(_response!.weatInfo.iconInfo);
+    });
+  }
+}
---
+++ lib/models.dart
@@ -0,0 +1,73 @@
+/*
+{
+  "weather": [
+    {
+      "main": "Clear",
+      "icon": "01d"
+    }
+  ],
+  "main": {
+    "temp": 282.55,
+    "temp_min": 280.37,
+    "temp_max": 284.26,
+    "pressure": 1023,
+    "humidity": 100
+  },
+  
+  }
+*/
+
+import 'package:flutter/material.dart';
+import 'package:gdg_school/main.dart';
+
+class WeatherInfo {
+  final String mainInfo;
+  final String iconInfo;
+  WeatherInfo({required this.mainInfo, required this.iconInfo});
+  factory WeatherInfo.fromJson(Map<String, dynamic> json) {
+    final mainInfo = json['main'];
+    final iconInfo = json['icon'];
+    return WeatherInfo(mainInfo: mainInfo, iconInfo: iconInfo);
+  }
+}
+
+class TempuratureInfo {
+  final double tempInfo;
+  final double maxInfo;
+  final double minInfo;
+  final int humidityInfo;
+  TempuratureInfo(
+      {required this.tempInfo,
+      required this.maxInfo,
+      required this.minInfo,
+      required this.humidityInfo});
+  factory TempuratureInfo.fromJson(Map<String, dynamic> json) {
+    final tempInfo = json['temp'];
+    final maxInfo = json['temp_max'];
+    final minInfo = json['temp_min'];
+    final humidityInfo = json['humidity'];
+    return TempuratureInfo(
+        tempInfo: tempInfo,
+        maxInfo: maxInfo,
+        minInfo: minInfo,
+        humidityInfo: humidityInfo);
+  }
+}
+
+class WeatherResponse {
+  final String cityName;
+  final TempuratureInfo tempInfo;
+  final WeatherInfo weatInfo;
+  WeatherResponse(
+      {required this.cityName, required this.tempInfo, required this.weatInfo});
+
+  factory WeatherResponse.fromJson(Map<String, dynamic> json) {
+    final cityName = json['name'];
+    final tempInfojson = json['main'];
+    final tempInfo = TempuratureInfo.fromJson(tempInfojson);
+    final WeatherInfojson = json['weather'][0];
+    final weatInfo = WeatherInfo.fromJson(WeatherInfojson);
+    return WeatherResponse(
+        cityName: cityName, tempInfo: tempInfo, weatInfo: weatInfo);
+  }
+}
